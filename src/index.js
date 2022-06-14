ed86f91c241e0f25683df48ec250936693d74e2c
isabelklint <54295403+isabelklint@users.noreply.github.com>
2022-06-13 19:51:23 -0500

    updated show temperature and weather description

--- src/index.js
+++ src/index.js
@@ -31,73 +31,70 @@ if (min < 10) {
 
 // CITY NAME
 
-// SET CITY NAME FROM SEARCH BAR INPUT
-function setCityNameAction(event) {
-  event.preventDefault();
-  let input = document.querySelector("#searchbar");
-  cityUpper = input.value.toLocaleUpperCase();
-  document.getElementById("cityName").innerHTML = `${cityUpper}`;
-  // SET TEMP
- // setTemp(cityLower);
-}
+// add event listeners to form
 let form = document.querySelector("form");
 form.addEventListener("submit", setCityNameAction);
-form.addEventListener("submit", getITemp);
 
-// SET CITY NAME FROM CURRENT LOCATION BUTTON
-// IGNORE ERROR 'REQUIRE IS NOT DEFINED'
+// add event listeners to location button
 location_button = document.querySelector("#location_button");
 location_button.addEventListener("click", getCityNameAction);
-location_button.addEventListener("click", getLTemp);
 
+// SET CITY NAME
+function setCityNameAction(event) {
+  event.preventDefault();
+  // get the input city name
+  let input = document.querySelector("#searchbar");
+  let cityUpper = input.value.toLocaleUpperCase();
+  // set the input city name
+  document.getElementById("cityName").innerHTML = `${cityUpper}`;
+  // make the URL
+    let apiKey = "af299e40c9c7667df5a6bc3d09004719";
+    let units = "imperial";
+    let cityLower = input.value.toLocaleLowerCase();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityLower}&appid=${apiKey}&units=${units}`;
+    // get the temperature
+    axios.get(`${apiUrl}`).then(setTemp);
+}
+
+// GET CITY NAME
 function getCityNameAction(event) {
-  navigator.geolocation.getCurrentPosition(showPosition);
+  // get the POSITION info
   event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getPosition);
 }
 
-
-function showCurrentLocation(response) {
-  let myCity = document.querySelector("#cityName");
-  myCity.innerHTML = response.data[0].name;
-  console.log("You changed the city name to current location.")
-}
-
-function showPosition(position) {
+function getPosition(position) {
+  // make the URL
   let apiKey = "af299e40c9c7667df5a6bc3d09004719";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
+  let units = "imperial";
   let apiUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  axios.get(`${apiUrl}`).then(showCurrentLocation);
+  // get the city name then change it
+  axios.get(`${apiUrl}`).then(changeCityName);
+  // change the temp
+  let apiUrl2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  axios.get(`${apiUrl2}`).then(setTemp);
 }
 
-// SET L TEMP
-function getLTemp(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(getWeatherFromLocation);
+function changeCityName(response) {
+  // get the input city name
+  let myCity = document.querySelector("#cityName");
+  myCity.innerHTML = response.data[0].name
 }
 
-function showTemperature(response) {
-  document.getElementById("fTemp").innerHTML = Math.round(response.data.main.temp) + "°f";
-  document.getElementById("cTemp").innerHTML = Math.round((response.data.main.temp - 32) / 1.8) + "°c";
-}
-
-function getWeatherFromLocation(position) {
-  let apiKey = "af299e40c9c7667df5a6bc3d09004719";
-  let units = "imperial";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-  axios.get(`${apiUrl}`).then(showTemperature);
-}
-
-// SET I TEMP
-function getITemp(event) {
-  event.preventDefault();
-  event.preventDefault();
-  let apiKey = "af299e40c9c7667df5a6bc3d09004719";
-  let units = "imperial";
-  let city = document.querySelector("#searchbar");
-  cityLower = city.value.toLocaleLowerCase();
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityLower}&appid=${apiKey}&units=${units}`;
-  axios.get(`${apiUrl}`).then(showTemperature);
+// SET TEMP
+function setTemp(response) {
+  console.log(response);
+  let tempF = document.querySelector("#fTemp");
+  let someF = Math.round(response.data.main.temp);
+  tempF.innerHTML = `${someF}˚f`;
+  let someC = Math.round((someF - 32) / 1.8);
+  let tempC = document.querySelector("#cTemp");
+  tempC.innerHTML = `${someC}˚c`;
+  let weather = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  let wind = document.querySelector("#wind");
+  let windspeed = Math.round(response.data.wind.speed);
+  wind.innerHTML = `${windspeed}mph`;
 }
